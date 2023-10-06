8471320deee7cb15047091fded03ad71249fa71f
Champ Henry Duco <ducochamp@gmail.com>
2023-10-06 16:29:11 +0800

    initial commit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,54 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+//@RequestMapping("/calculator")
 public class DemoController {
-    @GetMapping("/welcome")
-    public String welcome() {
-        return "Welcome Champ Doox";
+
+
+    @Autowired
+    private DemoService demoService;
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("notif", "No numbers to add");
+        return "result";
+    }
+
+    @GetMapping("/add/{num1}/{num2}")
+    public String add(@PathVariable Integer num1, @PathVariable Integer num2, Model model) {
+
+        int result = demoService.add(num1, num2);
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        model.addAttribute("result", result);
+
+        return "result";
+
+    }
+
+//    @GetMapping("/add")
+//    public String addNumbers(
+//            @RequestParam(defaultValue = "0") int num1,
+//            @RequestParam(defaultValue = "0") int num2,
+//            Model model
+//    ) {
+//        int result = num1 + num2;
+//        model.addAttribute("num1", num1);
+//        model.addAttribute("num2", num2);
+//        model.addAttribute("result", result);
+//        return "addition-result";
+//    }
 
 }
--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -1,2 +1,11 @@
-package com.example.demo;public class DemoService {
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
 }
