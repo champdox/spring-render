0d64aa21932dcaf94501becfe5b17da564446ba6
Champ Henry Duco <ducochamp@gmail.com>
2024-05-03 18:57:21 +0800

    feat: added webhook event listener

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -44,6 +41,7 @@ public class DemoController {
         return "result";
 
     }
+
     @GetMapping(value = "/webhook", produces = "text/plain")
     public ResponseEntity<String> verifyWebhook(@RequestParam("hub.mode") String mode,
                                                 @RequestParam("hub.verify_token") String verifyToken,
@@ -55,6 +53,13 @@ public class DemoController {
         } else {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
         }
+    }
+
+    @PostMapping(value = "/webhook")
+    public ResponseEntity<String> receiveSubscription(@RequestBody String payload) {
+//        JSONObject jsonPayload = new JSONObject(payload);
+        System.out.println("Received webhook event: " + payload);
+        return ResponseEntity.ok("Webhook event received successfully.");
     }
 
 
